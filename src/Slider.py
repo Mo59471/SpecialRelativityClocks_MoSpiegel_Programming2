0a30f82aabc39e50758c525dbcefc67de074391e
Mo59471 <9674025@graniteschools.org>
2026-04-02 18:16:59 -0600

    Update Slider.py

--- src/Slider.py
+++ src/Slider.py
@@ -1,3 +1,14 @@
 class Slider:
     def __init__(self):
+        self.slideX = 0
+        self.v = 0
+        self.hovering = False
+        
+    def display(self):
+        pass
+    
+    def hover(self):
+        pass
+    
+    def slide(self, clicked):
         pass
