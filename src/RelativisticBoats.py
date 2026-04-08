75a240602dc706e41074976f8bc6204ead334ff3
Mo59471 <9674025@graniteschools.org>
2026-04-08 17:38:58 -0600

    Update RelativisticBoats.py

--- src/RelativisticBoats.py
+++ src/RelativisticBoats.py
@@ -5,23 +5,35 @@ from Checkbox import Checkbox
 from Clock import Clock
 from Slider import Slider
 
+pygame.display.init()
+
+screen = pygame.display.set_mode((1150,850))
+
+# Load images
+backImg = pygame.image.load("Background.png").convert_alpha()
+sidebarImg = pygame.image.load("Sidebar.png")
+sidebarOverlayImg = pygame.image.load("SidebarOverlay.png").convert_alpha()
+sidebarElementImg = pygame.image.load("SidebarElements.png").convert_alpha()
+
+
 def display(screen, clicked):
 
     # Draw background
-    backImg = pygame.image.load("Background.png")
     screen.blit(backImg, (0,0))
 
     # Draw sidebar
-    sidebarImg = pygame.image.load("Sidebar.png")
     screen.blit(sidebarImg, (0,0))
-    sidebarOverlayImg = pygame.image.load("SidebarOverlay.png")
     screen.blit(sidebarOverlayImg, (0,0))
+    screen.blit(sidebarElementImg, (0,0))
     
-    #Draw buttons
+    # Draw buttons
     for button in buttons:
         button.hover()
         button.display(screen, clicked)
-        
+    
+    # Draw and update slider
+    slider.display(screen, clicked)
+    
     # Update screen
     pygame.display.flip()
 
@@ -30,22 +42,28 @@ def getdt():
     pass
 
 # Initialize data
-screen = pygame.display.set_mode((1150,850))
 clicked = False
+showClocks = True
+lorentzC = True
+dt = 0
+clock = pygame.time.Clock()
 
 # Initialize button data
 buttonData = [
-    {"label" : "play", "x": 30, "y": 37, "w": 258, "h": 75, "img": pygame.image.load("StartButtonNorm.png"), 
-    "hovImg": pygame.image.load("StartButtonHov.png"), "clickImg": pygame.image.load("StartButtonClick.png")},
+    {"label" : "play", "x": 30, "y": 37, "w": 258, "h": 75, "img": pygame.image.load("StartButtonNorm.png").convert_alpha(), 
+    "hovImg": pygame.image.load("StartButtonHov.png").convert_alpha(), "clickImg": pygame.image.load("StartButtonClick.png").convert_alpha()},
     
-    {"label" : "reset", "x": 30, "y": 140, "w": 258, "h": 75, "img": pygame.image.load("ResetButtonNorm.png"), 
-    "hovImg": pygame.image.load("ResetButtonHov.png"), "clickImg": pygame.image.load("ResetButtonClick.png")},
+    {"label" : "reset", "x": 30, "y": 140, "w": 258, "h": 75, "img": pygame.image.load("ResetButtonNorm.png").convert_alpha(), 
+    "hovImg": pygame.image.load("ResetButtonHov.png").convert_alpha(), "clickImg": pygame.image.load("ResetButtonClick.png").convert_alpha()},
     
-    {"label" : "equations", "x": 30, "y": 725, "w": 258, "h": 75, "img": pygame.image.load("EquationButtonNorm.png"), 
-    "hovImg": pygame.image.load("EquationButtonHov.png"), "clickImg": pygame.image.load("EquationButtonClick.png")}
+    {"label" : "equations", "x": 30, "y": 725, "w": 258, "h": 75, "img": pygame.image.load("EquationButtonNorm.png").convert_alpha(), 
+    "hovImg": pygame.image.load("EquationButtonHov.png").convert_alpha(), "clickImg": pygame.image.load("EquationButtonClick.png").convert_alpha()}
             ]
 
 buttons = []
+
+# Initialize slider
+slider = Slider()
 
 # Initialize buttons
 for data in buttonData:
@@ -57,12 +75,21 @@ while running:
 
     # Event Detection
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             running = False
+            
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 clicked = True
-                
+                mouseX, mouseY = pygame.mouse.get_pos()
+        
+                # Slider logic
+                if mouseX > slider.slideX + 25 and mouseX < slider.slideX + 75 and mouseY > 455 and mouseY < 505:
+                    slider.hovering = True       
+                else:
+                    slider.hovering = False
+                    
                 # Click logic
                 
         elif event.type == pygame.MOUSEBUTTONUP:
@@ -71,3 +98,5 @@ while running:
 
     # Method calls
     display(screen, clicked)
+    
+    clock.tick(60)
